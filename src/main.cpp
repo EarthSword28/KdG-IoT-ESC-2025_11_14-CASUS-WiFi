10ee876d470eb33a089d72fb573316ca1aba3851
EarthSword28 <jorden.van.nunen@gmail.com>
2025-11-14 10:52:44 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+
+#include <config.h>
+#include <secrets.h>
+
+#include <WiFi.h>
+
+void setup() {}
+
+void loop() {}
---
+++ src/secrets.h
@@ -0,0 +1,5 @@
+// WiFi
+const int KNOWN_WIFI_AMOUNT = 3;
+char *KNOWN_WIFI_SSIDs[] = {"SSID-1", "SSID-2", "SSID-3"};
+char *KNOWN_WIFI_PASSWORDs[] = {"PASSWORD-1", "PASSWORD-2", "PASSWORD-3"};
+const char* WIFI_HOST_NAME = "Example Host Name";
